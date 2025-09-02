7a4541243fdfeb522aa12d41ebf40d5d185cc10e
Curtis Wee <curtisw2e@yahoo.com.sg>
2025-09-02 04:05:53 +0200

    Fixed framebuffer 0 crash
    Fixed framebuffer crashing when browser is minimized

--- Ermine-Engine/src/Utility/EditorCamera.cpp
+++ Ermine-Engine/src/Utility/EditorCamera.cpp
@@ -75,7 +75,8 @@ void EditorCamera::SetViewportSize(float width, float height)
 
 #ifdef _DEBUG
 	// Update the frame buffer size for drawing on screen
-	ECS::GetInstance().GetSystem<graphics::Renderer>()->Create(static_cast<int>(width), static_cast<int>(height));
+	if (width > 0.f || height > 0.f)
+		ECS::GetInstance().GetSystem<graphics::Renderer>()->Create(static_cast<int>(width), static_cast<int>(height));
 #endif
 
 	UpdateProjectionMatrix();
