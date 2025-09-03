fa67283c842afdbc10f81ce2c4c6c265ec0a2189
hansi-sh <tansihan1@gmail.com>
2025-09-03 16:44:44 +0800

    move vector4d to math

--- Ermine-Engine/include/Math.h
+++ Ermine-Engine/include/Math.h
@@ -3,6 +3,7 @@
 /*!
 \file       Math.h
 \author     Tan Si Han, t.sihan, 2301264, t.sihan\@digipen.edu
+\co-authors WONG JUN YU, Kean, junyukean.wong, 2301234, junyukean.wong\@digipen.edu
 \date       Sept 02, 2025
 \brief      This file contains the declaration of the Math structure.
 
@@ -125,11 +126,6 @@ namespace Ermine
         Vector2D perpendicular() const;
 
     } Vector2D, Vec2;
-
-#ifdef _MSC_VER
-    // Supress warning: nonstandard extension used : nameless struct/union
-#pragma warning( default : 4201 )
-#endif
 
 #pragma region Binary operators
     /*!***********************************************************************
@@ -356,10 +352,6 @@ namespace Ermine
 
     /**********************************Vector3D***************************************/
 #pragma region Vector3D
-#ifdef _MSC_VER
-// Supress warning: nonstandard extension used : nameless struct/union
-#pragma warning( disable : 4201 )
-#endif
 
     /*!***********************************************************************
     \brief
@@ -459,11 +451,6 @@ namespace Ermine
         Vector3D operator-() const;
 
     } Vector3D, Vec3;
-
-#ifdef _MSC_VER
-    // Supress warning: nonstandard extension used : nameless struct/union
-#pragma warning( default : 4201 )
-#endif
 
 #pragma region Binary operators
     /*!***********************************************************************
@@ -711,4 +698,322 @@ namespace Ermine
 
 #pragma endregion Vector3D
     /**********************************Vector3D***************************************/
+
+    /**********************************Vector4D***************************************/
+#pragma region Vector4D
+    /*!***********************************************************************
+    \brief
+     Represents a 4D vector with x, y, z and w components, or as an array.
+    *************************************************************************/
+    typedef union Vector4D
+    {
+
+        struct
+        {
+            float x, y, z, w;
+        };
+
+        float m[4];
+
+        /*!***********************************************************************
+        \brief
+         Default constructor.
+        *************************************************************************/
+        Vector4D() : x(0), y(0), z(0), w(0) ,m { 0.0f } {}
+
+        /*!***********************************************************************
+        \brief
+         Constructor with x and y parameters.
+        \param[in] x
+         The x coordinate.
+        \param[in] y
+         The y coordinate.
+        \param[in] z
+         The z coordinate.
+        *************************************************************************/
+        Vector4D(float x, float y, float z, float w) : x(x), y(y), z(z), w(w), m{ x, y, z, w }
+        {
+        }
+
+        /*!***********************************************************************
+        \brief
+         Copy constructor.
+        \param[in] rhs
+         The vector to copy.
+        *************************************************************************/
+        Vector4D(const Vector4D& rhs) = default;
+
+        /*!***********************************************************************
+        \brief
+         Copy assignment operator.
+        \param[in] rhs
+         The vector to copy.
+        \return
+         A reference to the vector.
+        *************************************************************************/
+        Vector4D& operator=(const Vector4D& rhs) = default;
+
+        /*!***********************************************************************
+           \brief
+            Addition assignment operator.
+           \param[in] rhs
+            The vector to add.
+           \return
+            A reference to the vector.
+           *************************************************************************/
+        Vector4D& operator+=(const Vector4D& rhs);
+
+        /*!***********************************************************************
+        \brief
+         Subtraction assignment operator.
+        \param[in] rhs
+         The vector to subtract.
+        \return
+         A reference to the vector.
+        *************************************************************************/
+        Vector4D& operator-=(const Vector4D& rhs);
+
+        /*!***********************************************************************
+        \brief
+         Multiplication assignment operator.
+        \param[in] rhs
+         The scalar to multiply.
+        \return
+         A reference to the vector.
+        *************************************************************************/
+        Vector4D& operator*=(float rhs);
+
+        /*!***********************************************************************
+        \brief
+         Division assignment operator.
+        \param[in] rhs
+         The scalar to divide.
+        \return
+         A reference to the vector.
+        *************************************************************************/
+        Vector4D& operator/=(float rhs);
+
+        /*!***********************************************************************
+        \brief
+         Unary negation operator.
+        \return
+         The negated vector.
+        *************************************************************************/
+        Vector4D operator-() const;
+    } Vector4D, Vec4;
+
+    /*!***********************************************************************
+    \brief
+     Binary addition operator for two vectors.
+    \param[in] lhs
+     The first vector to add.
+    \param[in] rhs
+     The second vector to add.
+    \return
+     The vector of the sum of the two vectors.
+    *************************************************************************/
+    Vector4D operator+(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Binary subtraction operator for two vectors.
+    \param[in] lhs
+     The first vector to subtract.
+    \param[in] rhs
+     The second vector to subtract.
+    \return
+     The vector of the difference of the two vectors.
+    *************************************************************************/
+    Vector4D operator-(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Scalar multiplication operator for a vector and a scalar.
+    \param[in] lhs
+     The vector to multiply.
+    \param[in] rhs
+     The scalar to multiply.
+    \return
+     The vector of the product of the vector and scalar.
+    *************************************************************************/
+    Vector4D operator*(const Vector4D& lhs, float rhs);
+
+    /*!***********************************************************************
+    \brief
+     Scalar multiplication operator for a scalar and a vector.
+    \param[in] lhs
+     The scalar to multiply.
+    \param[in] rhs
+     The vector to multiply.
+    \return
+     The vector of the product of the scalar and vector.
+    *************************************************************************/
+    Vector4D operator*(float lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Scalar division operator for a vector and a scalar.
+    \param[in] lhs
+     The vector to divide.
+    \param[in] rhs
+     The scalar to divide.
+    \return
+     The vector of the division of the vector and scalar.
+    *************************************************************************/
+    Vector4D operator/(const Vector4D& lhs, float rhs);
+
+    /*!***********************************************************************
+    \brief
+     Compares two vectors for equality.
+    \param[in] lhs
+     The first vector.
+    \param[in] rhs
+     The second vector.
+    \return
+     True if all components are equal, false otherwise.
+    *************************************************************************/
+    bool operator==(const Vector4D& lhs, const Vector4D& rhs);
+    /*!***********************************************************************
+\brief
+ Compares two vectors for inequality.
+\param[in] lhs
+ The first vector.
+\param[in] rhs
+ The second vector.
+\return
+ True if any component differs, false otherwise.
+*************************************************************************/
+    bool operator!=(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Normalizes a vector.
+    \param[out] pResult
+     The normalized vector.
+    \param[in] vec
+     The input vector.
+    *************************************************************************/
+    void Vec4Normalize(Vector4D& pResult, const Vector4D& vec);
+
+    /*!***********************************************************************
+    \brief
+     Computes the length (magnitude) of a vector.
+    \param[in] vec
+     The input vector.
+    \return
+     The length of the vector.
+    *************************************************************************/
+    float Vec4Length(const Vector4D& vec);
+
+    /*!***********************************************************************
+    \brief
+     Computes the squared length of a vector.
+    \param[in] vec
+     The input vector.
+    \return
+     The squared length of the vector.
+    *************************************************************************/
+    float Vec4SquareLength(const Vector4D& vec);
+
+    /*!***********************************************************************
+    \brief
+     Computes the distance between two vectors.
+    \param[in] lhs
+     The first vector.
+    \param[in] rhs
+     The second vector.
+    \return
+     The Euclidean distance between lhs and rhs.
+    *************************************************************************/
+    float Vec4Distance(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Computes the squared distance between two vectors.
+    \param[in] lhs
+     The first vector.
+    \param[in] rhs
+     The second vector.
+    \return
+     The squared Euclidean distance between lhs and rhs.
+    *************************************************************************/
+    float Vec4SquareDistance(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Computes the dot product of two vectors.
+    \param[in] lhs
+     The first vector.
+    \param[in] rhs
+     The second vector.
+    \return
+     The dot product of lhs and rhs.
+    *************************************************************************/
+    float Vec4DotProduct(const Vector4D& lhs, const Vector4D& rhs);
+
+    /*!***********************************************************************
+    \brief
+     Linearly interpolates between two vectors.
+    \param[in] a
+     The start vector.
+    \param[in] b
+     The end vector.
+    \param[in] t
+     Interpolation factor (0.0f to 1.0f).
+    \return
+     The interpolated vector.
+    *************************************************************************/
+    Vector4D Vec4Lerp(const Vector4D& a, const Vector4D& b, float t);
+
+    /*!***********************************************************************
+    \brief
+     Clamps each component of a vector between minVal and maxVal.
+    \param[in] v
+     The input vector.
+    \param[in] minVal
+     Minimum value.
+    \param[in] maxVal
+     Maximum value.
+    \return
+     The clamped vector.
+    *************************************************************************/
+    Vector4D Vec4Clamp(const Vector4D& v, float minVal, float maxVal);
+
+    /*!***********************************************************************
+    \brief
+     Computes the angle (in radians) between two vectors.
+    \param[in] a
+     The first vector.
+    \param[in] b
+     The second vector.
+    \return
+     The angle between the vectors in radians.
+    *************************************************************************/
+    float Vec4Angle(const Vector4D& a, const Vector4D& b);
+
+    /*!***********************************************************************
+    \brief
+     Projects vector v onto vector onto.
+    \param[in] v
+     The vector to project.
+    \param[in] onto
+     The vector to project onto.
+    \return
+     The projected vector.
+    *************************************************************************/
+    Vector4D Vec4Project(const Vector4D& v, const Vector4D& onto);
+
+    /*!***********************************************************************
+    \brief
+     Reflects vector v about the given normal.
+    \param[in] v
+     The input vector.
+    \param[in] normal
+     The normal vector to reflect about.
+    \return
+     The reflected vector.
+    *************************************************************************/
+    Vector4D Vec4Reflect(const Vector4D& v, const Vector4D& normal);
+#pragma endregion Vector4D
+    /**********************************Vector4D***************************************/
 }
