053a08f7733a8a34b61aed7db34da0f40b69ae83
Kirroko\Kean <keanwng@gmail.com>
2025-09-09 09:14:14 +0800

    Updated standard to C++20

--- Ermine-Engine/src/Scripting/ScriptEngine.cpp
+++ Ermine-Engine/src/Scripting/ScriptEngine.cpp
@@ -33,8 +33,8 @@ void Ermine::scripting::ScriptEngine::InitMono(const std::string& assembly_path)
 		EE_CORE_ERROR("Failed to initialize Mono Core Domain");
 		return;
 	}
-
-	m_gameDomain = mono_domain_create_appdomain("ErmineGame", nullptr);
+	char gdn[] = "ErmineGame";
+	m_gameDomain = mono_domain_create_appdomain(gdn, nullptr);
 	mono_domain_set(m_gameDomain, true);
 
 	// Loading engine API assembly
