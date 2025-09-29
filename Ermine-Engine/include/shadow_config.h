c7a5caf0860ed1afdea11ca86302c168a8724903
ridhwanfandi <2301367@sit.singaporetech.edu.sg>
2025-09-29 14:09:30 +0800

    clean up

--- Ermine-Engine/include/shadow_config.h
+++ Ermine-Engine/include/shadow_config.h
@@ -3,7 +3,7 @@
 #define MAX_LIGHTS 32u
 #define NUM_CASCADES 4u
 #define SHADOW_MAX_LAYERS_DESIRED 16u // 32MB per layer for 4096x4096 (16 layers = 512MB) (about 4 lights with shadows, each with 4 cascades)
-extern unsigned int SHADOW_MAX_LAYERS;
+extern unsigned int SHADOW_MAX_LAYERS; // defined in Renderer.cpp
 #define SHADOW_MAP_RESOLUTION 4096u
 #define SHADOW_MAP_ARRAY_LAMBDA 0.95f
 #define SHADOW_MAp_REFRESH_INTERVAL 4 // 1 in 4 frames
--- Ermine-Engine/src/Graphics/Renderer.cpp
+++ Ermine-Engine/src/Graphics/Renderer.cpp
@@ -978,8 +978,8 @@ void Renderer::RenderPostProcessPass()
 void Renderer::RenderDeferredPipeline(const Mtx44& view, const Mtx44& projection)
 {
 	// Shadow pass - render scene from light's perspective
-	if (frameCounter % SHADOW_MAp_REFRESH_INTERVAL == 0) // Update shadows every 4 frames for performance
-	RenderShadowPass();
+	if (frameCounter % SHADOW_MAP_REFRESH_INTERVAL_IN_FRAMES == 0) // Update shadows every 4 frames for performance
+		RenderShadowPass();
 	// Geometry pass - write opaque objects to g-buffer, collect transparent objects
 	RenderGeometryPass(view, projection);
 
