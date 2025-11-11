f39687c102522e93e91f15d8578967c026dbf876
klivegoh <57471787+klivegoh@users.noreply.github.com>
2025-11-11 10:13:51 +0800

    testing scene with a switch script

---
+++ Ermine-ScriptSandbox/RotatingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ErmineEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public bool active = false;
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            transform.Rotate(Vector3.up * (Time.deltaTime * speed));
+        }
+    }
+
+    public void IsActive(bool state)
+    {
+        active = state;
+    }
+}
+
---
+++ Ermine-ScriptSandbox/Switch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ErmineEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool enabled = false;
+    public RotatingPlatform target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (enabled)
+        {
+            target.IsActive(enabled);
+        }
+    }
+}
+
