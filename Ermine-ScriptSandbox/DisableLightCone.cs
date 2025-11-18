7dbb4677886a1f97ccd2dd3e3c5ee6660c1a74d6
Abrin28 <brianleewj28@gmail.com>
2025-11-18 09:22:23 +0800

    Move light cone back after timer

--- Ermine-ScriptSandbox/DisableLightCone.cs
+++ Ermine-ScriptSandbox/DisableLightCone.cs
@@ -2,18 +2,35 @@
 
 public class DisableLightCone : MonoBehaviour
 {
+    public float timer = 3.0f;
+    private Vector3 oldPos;
+    private bool hit = false;
+
     private void Start()
     {
+        oldPos = transform.position;
     }
 
     private void Update()
     {
+        if (hit)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0.0f)
+            {
+                gameObject.transform.position = oldPos;
+                timer = 3.0f;
+                hit = false;
+            }
+        }
     }
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "Sphere")
         {
+            hit = true;
             //gameObject.SetActive(false);
             gameObject.transform.position = new Vector3(0, 30, 0);
             Physics.SetPosition((ulong)gameObject.GetInstanceID(), gameObject.transform.position);
