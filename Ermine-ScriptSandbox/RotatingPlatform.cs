1fca3054c819bf6881bc83758098785a2c794292
GOHKlive <goh.k@digipen.edu>
2025-11-22 16:44:17 +0800

    testing rotating platform

--- Ermine-ScriptSandbox/RotatingPlatform.cs
+++ Ermine-ScriptSandbox/RotatingPlatform.cs
@@ -6,17 +6,25 @@ public class RotatingPlatform : MonoBehaviour
     public float speed = 2.0f;
     public bool active = false;
 
+    private Rigidbody rb;
+
     void Start()
     {
-        Debug.Log("Hello World");
+        rb = GetComponent<Rigidbody>();
+        
+
+        
     }
 
     void Update()
     {
-        if (active)
-        {
-            transform.Rotate(Vector3.up * (Time.deltaTime * speed));
-        }
+        if (!active) return;
+
+        // Create a rotation for this frame
+        Quaternion delta = Quaternion.Euler(0f, speed * Time.deltaTime, 0f);
+
+        // Apply rotation manually (like MoveRotation)
+        rb.rotation = rb.rotation * delta;
     }
 
     public void IsActive(bool state)
--- Ermine-ScriptSandbox/Switch.cs
+++ Ermine-ScriptSandbox/Switch.cs
@@ -4,18 +4,26 @@ using ErmineEngine;
 public class Switch : MonoBehaviour
 {
     public bool enabled = false;
-    public RotatingPlatform target;
+    public GameObject target;
 
     void Start()
     {
-        
+        target = GameObject.Find("MyObject");
+        if (target == null)
+        {
+            Debug.Log("MyObject NOT found!");
+        }
+        else
+        {
+            Debug.Log("MyObject FOUND: " + target.name);
+        }
     }
 
     void Update()
     {
         if (enabled)
         {
-            target.IsActive(enabled);
+            target.GetComponent<RotatingPlatform>().IsActive(enabled);
         }
     }
 }
