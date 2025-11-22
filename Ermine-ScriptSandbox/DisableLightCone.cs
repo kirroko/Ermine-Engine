8f0d36d8cde48035d9dd65d277538e42327d9bbb
Abrin28 <brianleewj28@gmail.com>
2025-11-22 14:57:20 +0800

    Shoot near the top of lightcone to disable it

--- Ermine-ScriptSandbox/DisableLightCone.cs
+++ Ermine-ScriptSandbox/DisableLightCone.cs
@@ -4,12 +4,20 @@ public class DisableLightCone : MonoBehaviour
 {
     public float timer = 3.0f;
     private Vector3 oldPos;
+    private Vector3 oldConePos;
     private bool disabled = false;
     private bool orbInside = false;
+
+    private GameObject lightCone = null;
 
     private void Start()
     {
         oldPos = transform.position;
+
+        // Find matching LightConeX
+        lightCone = FindMatchingCone();
+        if (lightCone != null)
+            oldConePos = lightCone.transform.position;
     }
 
     private void Update()
@@ -17,7 +25,7 @@ public class DisableLightCone : MonoBehaviour
         // orb burst
         if (!disabled && orbInside && Input.GetMouseButtonDown(0))
         {
-            DisableCone();
+            DisableLight();
         }
 
         // if u want the lightcone to come back after disabling
@@ -27,15 +35,12 @@ public class DisableLightCone : MonoBehaviour
 
             if (timer <= 0.0f)
             {
-                gameObject.transform.position = oldPos;
-                Physics.SetPosition((ulong)gameObject.GetInstanceID(), gameObject.transform.position);
-                timer = 3.0f;
-                disabled = false;
+                RespawnLight();
             }
         }
     }
 
-    private void DisableCone()
+    private void DisableLight()
     {
         disabled = true;
         //gameObject.SetActive(false);
@@ -43,6 +48,30 @@ public class DisableLightCone : MonoBehaviour
         // we move the lightcone out of view for now until SetActive() is implemented
         gameObject.transform.position = new Vector3(0, oldPos.y + 100, 0);
         Physics.SetPosition((ulong)gameObject.GetInstanceID(), gameObject.transform.position);
+
+        // Disable linked cone too
+        if (lightCone != null)
+        {
+            lightCone.transform.position = new Vector3(0, oldPos.y + 100, 0);
+            Physics.SetPosition((ulong)lightCone.GetInstanceID(), lightCone.transform.position);
+        }
+    }
+
+    void RespawnLight()
+    {
+        disabled = false;
+        timer = 3.0f;
+
+        // Respawn original light
+        gameObject.transform.position = oldPos;
+        Physics.SetPosition((ulong)gameObject.GetInstanceID(), gameObject.transform.position);
+
+        // Respawn cone in original place
+        if (lightCone != null)
+        {
+            lightCone.transform.position = oldConePos;
+            Physics.SetPosition((ulong)lightCone.GetInstanceID(), lightCone.transform.position);
+        }
     }
 
     void OnCollisionEnter(Collision col)
@@ -59,5 +88,31 @@ public class DisableLightCone : MonoBehaviour
         {
             orbInside = false;
         }
+    }
+
+    private GameObject FindMatchingCone()
+    {
+        string name = gameObject.name;
+        string number = "";
+
+        // Read digits from the end of the string
+        for (int i = name.Length - 1; i >= 0; i--)
+        {
+            char c = name[i];
+            if (c >= '0' && c <= '9')
+            {
+                number = c + number;
+            }
+            else
+            {
+                break; // stop when we hit first non-digit
+            }
+        }
+
+        if (number == "")
+            return null; // no trailing number found
+
+        string coneName = "LightCone" + number;
+        return GameObject.Find(coneName);
     }
 }
