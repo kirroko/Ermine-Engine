737a720c8671045d43ecb7a483f27bf9ddc88c63
hansi-sh <tansihan1@gmail.com>
2025-11-25 14:15:48 +0800

    fix jump

--- Ermine-Engine/src/Scripting/ScriptEngine.cpp
+++ Ermine-Engine/src/Scripting/ScriptEngine.cpp
@@ -1995,6 +1995,11 @@ namespace
 		auto physics = ECS::GetInstance().GetSystem<Physics>();
 		physics->RemovePhysic((EntityID)entityID);
 	}
+	static void icall_Physics_Jump(uint64_t entityID,float jump)
+	{
+		auto physics = ECS::GetInstance().GetSystem<Physics>();
+		physics->Jump((EntityID)entityID,jump);
+	}
 
 #pragma endregion
 
@@ -2496,6 +2501,7 @@ void Ermine::scripting::ScriptEngine::RegisterInternalCalls() const
 	mono_add_internal_call("ErmineEngine.Physics::Internal_Raycast", (const void*)icall_physics_raycast);
 	mono_add_internal_call("ErmineEngine.Physics.RaycastHit::get_transform", (const void*)icall_gameobject_get_transform);
 	mono_add_internal_call("ErmineEngine.Physics::RemovePhysic", (const void*)&icall_Physics_RemovePhysic);
+	mono_add_internal_call("ErmineEngine.Physics::Jump", (const void*)icall_Physics_Jump);
 #pragma endregion
 #pragma region UI ICalls
 	mono_add_internal_call("ErmineEngine.GameplayHUD::Internal_GetHealth", (const void*)Internal_GetHealth);
