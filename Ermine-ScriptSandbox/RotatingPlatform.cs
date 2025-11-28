1ab40e60ea4cd3e76bec51f6874aee874b4061c3
hansi-sh <tansihan1@gmail.com>
2025-11-28 22:41:17 +0800

    rotating platform for player (WIP)

--- Ermine-ScriptSandbox/RotatingPlatform.cs
+++ Ermine-ScriptSandbox/RotatingPlatform.cs
@@ -13,6 +13,9 @@ public class RotatingPlatform : MonoBehaviour
     private AudioComponent audioComp;
     private GameObject player;
 
+    private float lastYRot;
+    private bool PlayerIsOnPlatform = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -28,6 +31,7 @@ public class RotatingPlatform : MonoBehaviour
         {
             Console.WriteLine("Warning: Player not found!");
         }
+        lastYRot = transform.rotation.eulerAngles.y;
     }
 
     void Update()
@@ -42,10 +46,42 @@ public class RotatingPlatform : MonoBehaviour
             return;
         }
 
+        float prevY = transform.rotation.eulerAngles.y;
+
         // Apply rotation
         transform.Rotate(new Vector3(0f, speed * Time.deltaTime, 0f));
         Physics.SetRotationQuat((ulong)gameObject.GetInstanceID(), transform.rotation);
-        
+
+        float currentY = transform.rotation.eulerAngles.y;
+        float deltaAngle = currentY - lastYRot;
+
+        // Wrap around 360
+        if (deltaAngle > 180) deltaAngle -= 360;
+        if (deltaAngle < -180) deltaAngle += 360;
+
+        float radians = -deltaAngle * Mathf.Deg2Rad;
+
+        if (PlayerIsOnPlatform)
+        {
+            Vector3 pos = player.transform.position;
+            Vector3 center = transform.position;
+
+            float dx = pos.x - center.x;
+            float dz = pos.z - center.z;
+
+            float cos = Mathf.Cos(radians);
+            float sin = Mathf.Sin(radians);
+
+            float newX = dx * cos - dz * sin;
+            float newZ = dx * sin + dz * cos;
+
+            pos.x = center.x + newX;
+            pos.z = center.z + newZ;
+
+            player.transform.position = pos;
+            Physics.SetPosition((ulong)player.GetInstanceID(), pos);
+        }
+
         // Handle audio based on player distance
         HandleAudioByDistance();
     }
@@ -88,4 +124,26 @@ public class RotatingPlatform : MonoBehaviour
             }
         }
     }
+    void OnCollisionEnter(Collider other)
+    {
+
+    }
+    void OnCollisionStay(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            PlayerIsOnPlatform = true;
+        }
+    }
+
+
+    void OnCollisionExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            PlayerIsOnPlatform = false;
+            Debug.Log("PlayerIsOnPlatform false");
+        }
+    }
+
 }
