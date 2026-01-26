1d27f4364b92d05ff9c1fa0648f672799fd8652a
hansi-sh <tansihan1@gmail.com>
2026-01-26 13:30:45 +0800

    added motiontype check and sphere collision check

--- Ermine-Engine/src/Scripting/ScriptEngine.cpp
+++ Ermine-Engine/src/Scripting/ScriptEngine.cpp
@@ -2482,6 +2482,11 @@ namespace
 		auto physics = ECS::GetInstance().GetSystem<Physics>();
 		physics->Jump((EntityID)entityID,jump);
 	}
+	static int icall_Physics_CheckMotionType(uint64_t entityID)
+	{
+		auto physics = ECS::GetInstance().GetSystem<Physics>();
+		return physics->GetMotionType((EntityID)entityID);
+	}
 	static void icall_Physics_ForceUpdate()
 	{
 		auto physics = ECS::GetInstance().GetSystem<Physics>();
@@ -3103,6 +3108,7 @@ void Ermine::scripting::ScriptEngine::RegisterInternalCalls() const
 	mono_add_internal_call("ErmineEngine.Physics::Internal_GetRigidbody", (const void*)icall_rigidbody_get_rigidbody);
 	mono_add_internal_call("ErmineEngine.Physics::RemovePhysic", (const void*)&icall_Physics_RemovePhysic);
 	mono_add_internal_call("ErmineEngine.Physics::Jump", (const void*)icall_Physics_Jump);
+	mono_add_internal_call("ErmineEngine.Physics::CheckMotionType", (const int*)icall_Physics_CheckMotionType);
 	mono_add_internal_call("ErmineEngine.Physics::ForceUpdate", (const void*)icall_Physics_ForceUpdate);
 #pragma endregion
 
