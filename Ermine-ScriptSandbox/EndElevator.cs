2c9dd3e058038631e2f411786689a29c5d367443
GOHKlive <goh.k@digipen.edu>
2026-02-05 22:32:31 +0800

    EndElevator script

---
+++ Ermine-ScriptSandbox/EndElevator.cs
@@ -0,0 +1,20 @@
+﻿using ErmineEngine;
+
+public class EndElevator : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Trigger entered");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+    
+}
