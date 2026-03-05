d346c6e03529ab5f4d666e7e9a3f38ce7569d3c2
Abrin28 <brianleewj28@gmail.com>
2026-03-05 19:09:15 +0800

    stop fsm scripts from running when not playing

--- Ermine-Engine/src/FSM/FiniteStateMachine.cpp
+++ Ermine-Engine/src/FSM/FiniteStateMachine.cpp
@@ -25,6 +25,12 @@ namespace Ermine
 
     void StateManager::Update(float dt)
     {
+#if defined(EE_EDITOR)
+        // Only run FSM logic in Play mode
+        if (editor::EditorGUI::s_state != editor::EditorGUI::SimState::playing)
+            return;
+#endif
+
         for (auto entity : m_Entities)
         {
             if (!ECS::GetInstance().HasComponent<StateMachine>(entity))
