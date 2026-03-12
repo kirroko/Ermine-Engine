981a205080ac8dbe6310e677c665c6c0ec5c3522
hansi-sh <tansihan1@gmail.com>
2026-03-12 17:47:48 +0800

    Update SpotLightTrigger.cs

--- Ermine-ScriptSandbox/SpotLightTrigger.cs
+++ Ermine-ScriptSandbox/SpotLightTrigger.cs
@@ -36,7 +36,7 @@ public class SpotLightTrigger : MonoBehaviour
     void Update()
     {
         if (player == null) return;
-        if (Physics.Internal_GetLightValue((ulong)this.GetInstanceID()) == 0) return;
+        if (Physics.Internal_GetLightValue((ulong)gameObject.GetInstanceID()) == 0) return;
 
         bool inside = IsPointInsideSpot(player.transform.position);
 
