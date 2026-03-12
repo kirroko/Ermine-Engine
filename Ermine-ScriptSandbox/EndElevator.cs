f56305c78cf0ef9dc41a3a445f775f39e170e564
GOHKlive <goh.k@digipen.edu>
2026-03-13 02:09:38 +0800

    Randomise jail cell vertical movement Check only allow player to trigger elevator

--- Ermine-ScriptSandbox/EndElevator.cs
+++ Ermine-ScriptSandbox/EndElevator.cs
@@ -55,6 +55,7 @@ public class EndElevator : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.name != "Player") return;
         Debug.Log("Player entered the elevator");
         playerIn = true;
     }
